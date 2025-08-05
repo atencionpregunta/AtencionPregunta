8b7dcb1119022e00db54ba054c24ca197c164e47
atencionpregunta <atencionpregunta1@gmail.com>
2025-08-05 23:02:09 +0200

    a

--- app.py
+++ app.py
@@ -40,4 +40,5 @@ def aceptar():
 
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
